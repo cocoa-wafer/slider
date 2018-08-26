44145bcb7d91a27ae034180621c9474e8470df02
audreyg <audreyg@users.noreply.github.com>
2018-08-26 19:52:16 +0200

    maj poo

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,14 @@
-
-$(document).ready(function() {
-   
- 
-var $carrousel = $('#carrousel'),
-$img = $('#carrousel img'),
-$indexImg = $img.length-1,
-i=0,
-$currentImg = $img.eq(i);
-
-$img.css('display','none');
-$currentImg.css('display','block');
-
-
-$(document).keydown(function(e){
-        if (e.keyCode == 39) {
+$('document').ready(function(){
+    
+    var $img = $('#carrousel img'),
+    $indexImg = $img.length-1,
+    $currentImg = $img.eq(i);
+    var i=0;
+    var $document = $(document);
+    
+    var Slider = {
+        avancer : function() {         
+            
             i++; 
             if (i<=$indexImg) {
                 $img.css('display','none');
@@ -23,49 +18,59 @@ $(document).keydown(function(e){
             else {
                 i=$indexImg;
             } 
-        }       
-});
-
-
-
-$(document).keydown(function(e){
-        if (e.keyCode == 37) {
+        },
+        
+        reculer : function() {
+      
             i--;
             if (i>= 0) {
-            $img.css('display','none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display','block');
+                $img.css('display','none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display','block');
             }
             else {
-            i=0;
+                i=0;
             }
-        }
-});
-
-
-
-function defiler(){
-
-    setTimeout(function(){
+            
+        },
         
-        if (i<$indexImg) {
-            i++;
-            $img.css('display','none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display','block');
+        defiler: function() {
+            
+            setTimeout(function(){
+        
+                if (i<$indexImg) {
+                    i++;
+                    $img.css('display','none');
+                    $currentImg = $img.eq(i);
+                    $currentImg.css('display','block');
+                }
+                else {
+                    i=0;
+                    $img.css('display','none');
+                    $currentImg = $img.eq(i);
+                    $currentImg.css('display','block');
+                }    
+                Slider.defiler();  
+            }, 9000);
+        },
+        
+        init : function() {
+
+            Slider.defiler();
+            
+            $document.on('keydown',function(e) {
+                if (e.keyCode ==39) {
+                    Slider.avancer();
+                }
+                else if (e.keyCode ==37) {
+                    Slider.reculer();
+                }
+            });
         }
-        else {
-            i=0;
-            $img.css('display','none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display','block');
-        }    
-        defiler();  
-    }, 3000);
-}
 
-defiler();
-                 
-                  
+    };
+
+    Slider.init();
+    
+    
 });
-
