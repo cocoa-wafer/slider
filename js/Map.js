d593eb7d281b8d20831f9977be09ce4e3a821ac3
audreyg <audreyg@users.noreply.github.com>
2018-09-04 19:25:50 +0200

    markerclusterer

--- js/Map.js
+++ js/Map.js
@@ -1,25 +1,34 @@
 var Map = {
     
-    
+    map: "", 
+    markers: [],
     setMarker: function() {
-
-
 
          for (var i =0 ; i < Stations.latitude.length; i++) {
             var myLatlng = new google.maps.LatLng(Stations.latitude[i],Stations.longitude[i]);
             var marker = new google.maps.Marker({
                 position: myLatlng,
                 title:Stations.adresse[i],
-                setMap:document.getElementById('map')
+                map:map
             });
-
+            this.markers.push(marker);
         }; 
+
+        
+        var options = {
+            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        };
+
+        var markerCluster = new MarkerClusterer(map, this.markers, options);
+        
+        
+        
     },
     
   initMap: function() {
       
      var lyon = {lat:45.764043, lng:4.835658999999964};
-     var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: lyon});
+     map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: lyon});
 
       
   }
