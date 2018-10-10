a16844d063bf5fdbe803aea3e270ac19982cb612
audreyg <audreyg@users.noreply.github.com>
2018-10-10 16:01:38 +0200

    refacto

--- js/Canva.js
+++ js/Canva.js
@@ -1,6 +1,8 @@
 var Canva = {
     myVar : "",
     signerElt:"",
+    velo:"",
+    station:"",
     
     getMousePos:function(canvas, evt) {
         var rect = canvas.getBoundingClientRect();
@@ -91,6 +93,40 @@ var Canva = {
         document.getElementById('current').appendChild(annulerElt);
         
     },
+    
+    diminuerCompteur:function() {
+                         
+        var e = new Date();
+        var resaTimestamp = localStorage.getItem('time');
+        var f= e.getTime();
+                           
+                           
+        // difference en ms entre resa et maintenant
+        var difference = f - resaTimestamp;
+        //un temps en millisecondes
+        var t = (difference);
+        //secondes
+        var s = Math.floor(t / 1000) % 60;
+        //minutes
+        var m = Math.floor(t / 60000) % 60;
+        //affichage
+        var chaine = m+":"+s+ "minutes";
+        document.getElementById('compteur').innerHTML = chaine;
+        localStorage.setItem('difference',difference);   
+                            
+        if (difference >= 1200000) {
+            document.getElementById('timer').innerHTML = "réservation expirée";
+            Canva.Stop();
+            localStorage.removeItem('station');
+            localStorage.removeItem('velo');
+            localStorage.removeItem('time');
+            localStorage.removeItem('difference');
+            document.getElementById('current').innerHTML="";
+            document.getElementById('formulaire').appendChild(Canva.signerElt);
+
+        }            
+        
+    },
 
     
     timer:function() {
@@ -109,167 +145,113 @@ var Canva = {
         var compteurElt = document.createElement('span');
         compteurElt.id = "compteur";
         document.getElementById('timeLeft').appendChild(compteurElt);
-                  
-        // Diminue le compteur jusqu'à 0
-        function diminuerCompteur() {
-                         
-            var e = new Date();
-            var resaTimestamp = localStorage.getItem('time');
-            var f= e.getTime();
-                           
-                           
-            // difference en ms entre resa et maintenant
-            var difference = f - resaTimestamp;
-            //un temps en millisecondes
-            var t = (difference);
-            //secondes
-            var s = Math.floor(t / 1000) % 60;
-            //minutes
-            var m = Math.floor(t / 60000) % 60;
-            //affichage
-            var chaine = m+":"+s+ "minutes";
-            compteurElt.innerHTML = chaine;
-            localStorage.setItem('difference',difference);   
-                            
-            if (difference >= 1200000) {
-                document.getElementById('timer').innerHTML = "réservation expirée";
-                Canva.Stop();
-                localStorage.removeItem('station');
-                localStorage.removeItem('velo');
-                localStorage.removeItem('time');
-                localStorage.removeItem('difference');
-                document.getElementById('current').innerHTML="";
-                document.getElementById('formulaire').appendChild(Canva.signerElt);
 
-            }                 
-                          
-        }
-                        
-        Canva.myVar = setInterval(diminuerCompteur,1000); 
-
+        
+        Canva.myVar = setInterval(Canva.diminuerCompteur,1000); 
     },
     
-Stop: function() {
+    Stop: function() {
       clearInterval(Canva.myVar);  
     },
     
-
-
-    
-signature: function() {
-    
-    //affiche le form
-    var formElt = document.createElement('form');
-    formElt.id="formulaire";
-    var prenomElt = document.createElement('p');
-    var prenomInput = document.createElement('input');
-    prenomInput.id="prenom";
-    prenomElt.textContent = "Prénom: " ; 
-    prenomElt.appendChild(prenomInput);
+    formulaire:function() {
+        
+        //affiche le form
+        var formElt = document.createElement('form');
+        formElt.id="formulaire";
+        var prenomElt = document.createElement('p');
+        var prenomInput = document.createElement('input');
+        prenomInput.id="prenom";
+        prenomElt.textContent = "Prénom: " ; 
+        prenomElt.appendChild(prenomInput);
             
-    var nomElt = document.createElement('p');
-    var nomInput = document.createElement('input');
-    nomInput.id="nom";
-    nomElt.textContent = "Nom: ";
-    nomElt.appendChild(nomInput);
-    Canva.signerElt = document.createElement('input');
-    Canva.signerElt.id="signer";
-    Canva.signerElt.type="button";
-    Canva.signerElt.value="signer";
+        var nomElt = document.createElement('p');
+        var nomInput = document.createElement('input');
+        nomInput.id="nom";
+        nomElt.textContent = "Nom: ";
+        nomElt.appendChild(nomInput);
+        Canva.signerElt = document.createElement('input');
+        Canva.signerElt.id="signer";
+        Canva.signerElt.type="button";
+        Canva.signerElt.value="signer";
             
-    formElt.appendChild(prenomElt);
-    formElt.appendChild(nomElt);
-    formElt.appendChild(Canva.signerElt);
+        formElt.appendChild(prenomElt);
+        formElt.appendChild(nomElt);
+        formElt.appendChild(Canva.signerElt);
             
-    document.getElementById('resa').appendChild(formElt);
+        document.getElementById('resa').appendChild(formElt);
 
-    //local sotrage nom prenom 
+        //local sotrage nom prenom 
     
-    if (localStorage.getItem('prenom')) {
-        document.getElementById('prenom').value = localStorage.getItem('prenom');
-        document.getElementById('nom').value= localStorage.getItem('nom');
-    }; 
+        if (localStorage.getItem('prenom')) {
+            document.getElementById('prenom').value = localStorage.getItem('prenom');
+            document.getElementById('nom').value= localStorage.getItem('nom');
+        }; 
     
-    document.getElementById('current').innerHTML="";
-    
-    $('#signer').on('click',function(e) {
-        e.preventDefault();
-        document.getElementById('formulaire').removeChild(Canva.signerElt);
-        Canva.initResa();
-                
+    },
 
+
+    
+    signature: function() {
+    
+        Canva.formulaire();
+        document.getElementById('current').innerHTML="";
+    
+        $('#signer').on('click',function(e) {
+            e.preventDefault();
+            document.getElementById('formulaire').removeChild(Canva.signerElt);
+            Canva.initResa();    
+            Canva.resa();
+        });
+
+    },
+    
+    resa:function() {
+    
         $('#reserver').on('click',function(e){
             e.preventDefault();
-            //stocke dans le storage
             prenom = $('#prenom').val();
             nom = $('#nom').val();
             localStorage.setItem('prenom',prenom);
             localStorage.setItem('nom',nom);
 
-
             if (localStorage.getItem('station')) {
-   
-                //affiche annuler plus mesqage deja reserve en plus 
+
                 Canva.initAnnulation();
                 var attentionElt = document.createElement('p');
                 attentionElt.textContent ="station déjà réservée en attente";
                 attentionElt.id="attention";
                 document.getElementById('current').appendChild(attentionElt);
                 document.getElementById('attention').style.color = "red";
-
+                
                 $('#annuler').on('click',function(){
-                            
-                    //affiche fonction reserver plus message annule plus un velo en plus
-                    velo = velo+1;
-                    Canva.Stop();
-                    localStorage.removeItem('station');
-                    localStorage.removeItem('velo'); 
-                    localStorage.removeItem('difference'); 
-                    localStorage.removeItem('time');
-                    document.getElementById('current').removeChild(attentionElt);
-                    document.getElementById('timer').textContent = "Reservation annulée";
-                    document.getElementById('formulaire').appendChild(Canva.signerElt);
-                    document.getElementById('current').innerHTML="";
-
+                    Canva.annulResa();
                 });
+                
                     
                     
             } else {
                         
                 if ($('#canva').hasClass('active')) {
-                    
                     if ((localStorage.getItem('prenom').length >=1) && (localStorage.getItem('nom').length>=1) ){
                         
                         document.getElementById('erreur').innerHTML="";
-                        //recup infos station et velo
-                        var velo = Map.velo;
-                        var station = Map.station;
-                        //affiche fonction annuler
+                        Canva.velo = Map.velo;
+                        Canva.station = Map.station;
                         document.getElementById('timer').innerHTML="";
                         Canva.initAnnulation();
-                        //stocke dans le storage
-                        localStorage.setItem('velo',velo);
-                        localStorage.setItem('station',station);
+                        localStorage.setItem('velo',Canva.velo);
+                        localStorage.setItem('station',Canva.station);
                         var resa = new Date();
                         var timestamp = resa.getTime();
                         localStorage.setItem('time', timestamp);
-                        velo= velo-1;
-                        document.getElementById('velos').textContent = "velos disponibles: " + velo;    
-                        
+                        Canva.velo= Canva.velo-1;
+                        document.getElementById('velos').textContent = "velos disponibles: " + Canva.velo;    
                         Canva.timer();
 
                         $('#annuler').on('click',function(){
-                            //affiche fonction reserver
-                            document.getElementById('formulaire').appendChild(Canva.signerElt);
-                            document.getElementById('current').innerHTML="";
-                            velo = velo+1;
-                            document.getElementById('velos').textContent = "velos dispos: " + velo;
-                            Canva.Stop();
-                            localStorage.removeItem('station');
-                            localStorage.removeItem('velo'); 
-                            localStorage.removeItem('difference'); 
-                            localStorage.removeItem('time');                    
-                            document.getElementById('timer').textContent = "Reservation annulée";
+                            Canva.annulResa();
+                            document.getElementById('velos').textContent = "velos disponibles: " + Canva.velo;
                    
                         });
                         
@@ -286,12 +268,21 @@ signature: function() {
      
             }
   
-        }); 
-                
-                
-    });
+        });   
+    },
 
+    annulResa:function() {
+        
+        Canva.Stop();
+        Canva.velo = Canva.velo+1;
+        localStorage.removeItem('station');
+        localStorage.removeItem('velo'); 
+        localStorage.removeItem('difference'); 
+        localStorage.removeItem('time');
+        document.getElementById('timer').textContent = "Reservation annulée";
+        document.getElementById('formulaire').appendChild(Canva.signerElt);
+        document.getElementById('current').innerHTML="";
 
-}
+    }
     
 };
